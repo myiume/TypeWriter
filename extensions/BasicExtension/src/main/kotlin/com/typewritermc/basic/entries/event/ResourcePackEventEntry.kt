2ed0e8d8a70b43da8930dadf870d6aa5e22d26f0
Octavian Ciubotaru <myiume@gmail.com>
2025-01-09 17:38:34 +0200

    Resource pack status event entry
    - Reworked resource pack event entry to use lists and get rid of enums
    - Changed after code review

--- extensions/BasicExtension/src/main/kotlin/com/typewritermc/basic/entries/event/ResourcePackEventEntry.kt
+++ extensions/BasicExtension/src/main/kotlin/com/typewritermc/basic/entries/event/ResourcePackEventEntry.kt
@@ -17,63 +17,26 @@ import kotlin.reflect.KClass
     "resource_pack_event",
     "When the player's resource pack status changes",
     Colors.YELLOW,
-    "mingcute:archive-fill"
+    "mingcute:file-cloud-fill"
 )
-@ContextKeys(ResourcePackContextKeys::class)
 class ResourcePackEventEntry(
     override val id: String = "",
     override val name: String = "",
     override val triggers: List<Ref<TriggerableEntry>> = emptyList(),
 
-    @Help("Which status triggers this event? (SUCCESSFULLY_LOADED by default).")
-    val statusFilter: ResourcePackStatusFilter = ResourcePackStatusFilter.SUCCESSFULLY_LOADED
+    @Help("List of resource pack statuses that will trigger this event.")
+    @Default("[\"SUCCESSFULLY_LOADED\"]")
+    val statuses: List<Status> = listOf(Status.SUCCESSFULLY_LOADED)
 
 ) : EventEntry
 
-
-enum class ResourcePackStatusFilter(vararg val statuses: Status) {
-    ANY(
-        Status.ACCEPTED,
-        Status.DECLINED,
-        Status.DISCARDED,
-        Status.DOWNLOADED,
-        Status.FAILED_DOWNLOAD,
-        Status.FAILED_RELOAD,
-        Status.INVALID_URL,
-        Status.SUCCESSFULLY_LOADED
-    ),
-    ACCEPTED(Status.ACCEPTED),
-    DECLINED(Status.DECLINED),
-    DISCARDED(Status.DISCARDED),
-    DOWNLOADED(Status.DOWNLOADED),
-    FAILED_DOWNLOAD(Status.FAILED_DOWNLOAD),
-    FAILED_RELOAD(Status.FAILED_RELOAD),
-    INVALID_URL(Status.INVALID_URL),
-    SUCCESSFULLY_LOADED(Status.SUCCESSFULLY_LOADED);
-
-    fun matches(eventStatus: Status) = eventStatus in statuses
-}
-
-enum class ResourcePackContextKeys(override val klass: KClass<*>) : EntryContextKey {
-    @KeyType(Status::class)
-    STATUS(Status::class),
-}
-
 @EntryListener(ResourcePackEventEntry::class)
-fun onResourcePackChange(
-    event: PlayerResourcePackStatusEvent,
-    query: Query<ResourcePackEventEntry>
-) {
-    val player = event.player
-    val status = event.status
+fun onResourcePackChange(event: PlayerResourcePackStatusEvent, query: Query<ResourcePackEventEntry>) {
+    val eventStatus = event.status
 
-    val entries = query.findWhere { entry ->
-        entry.statusFilter.matches(status)
-    }.toList()
+    val entries = query.findWhere { entry -> eventStatus in entry.statuses }.toList()
 
     if (entries.isEmpty()) return
 
-    entries.triggerAllFor(player, context())
+    entries.triggerAllFor(event.player, context())
 }
-
-
