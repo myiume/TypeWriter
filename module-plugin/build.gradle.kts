8b7c036e8110189c288c96ce868ea85042c89487
Gabber235 <gabber200@icloud.com>
2025-01-14 06:35:13 +0100

    Upgrade module plugin version to 1.1.2

--- module-plugin/build.gradle.kts
+++ module-plugin/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.typewritermc.module-plugin"
-version = "1.1.1"
+version = "1.1.2"
 
 val engineVersion = file("../version.txt").readText().trim().substringBefore("-beta")
 
